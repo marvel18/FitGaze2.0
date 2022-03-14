f46fc380c6d9323f348cb9e60b895ad2d8571122
MARVEL VARGHESE <marvelvarghese@MARVELs-MacBook-Air.local>
2022-03-14 19:47:39 +0530

    line bug solved

---
+++ main.js
@@ -0,0 +1,39 @@
+window.onload = function(){
+$('#fontslider').on('change', function() {
+    console.log('input');
+    computeLines()
+      var v= $(this).val();
+      $('div').css('font-size', v + 'px');
+    });
+$('#paddingslider').on('change', function() {
+            computeLines()
+          var v= $(this).val();
+          $('div').css('line-height', v + 'px')
+        });
+$(function(){
+    console.log("computelines");
+    var p = $('p'); 
+    var words = p.text().split(' '); 
+    var text = ''; 
+    $.each(words, function(i, w){
+                     if($.trim(w)) text = text + '<span>' + w + '</span> ' }
+          );
+    p.html(text); 
+    computeLines = function(){ 
+      var line = 0; 
+      var prevTop = -15; 
+      $('span', p).each(function(){ 
+        var word = $(this); 
+        var top = word.offset().top; 
+        if(top!=prevTop){ 
+          prevTop=top; 
+          line++; 
+        } 
+        word.attr('class', 'line' + line); 
+      });
+      
+    }
+    $(window).resize();
+  });
+$(window).resize(computeLines);
+}
